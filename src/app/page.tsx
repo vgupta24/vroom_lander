2084af22dab7bc017b1d6202559e9ae69fd96711
Vik Gupta <vikgupta@MacBook-Pro-8.local>
2026-02-26 13:08:17 -0500

    Update homepage blurb to software positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
             VROOM
           </h1>
           <p className="text-load-in mt-8 max-w-3xl mx-auto text-lg md:text-2xl text-gray-600 leading-relaxed">
-            VROOM is an event planning agency that leverages automation tools to
-            take care of event planning busywork. By solving the
+            VROOM is building event planning software that leverages automation
+            tools to take care of event planning busywork. By solving the
             back-and-forth between vendors, venue sourcing, and event logistics,
             allowing hosts to focus on the guest experience and the creative
             vision.
